220435dd730c453d7c7c677dc9ff95316ca58f2f
Sergi <sergi.granell.7e3@itb.cat>
2020-12-07 02:12:06 +0100

    moviment accidentament diagonal
    Per el moment es solament una prova, pero sembla que funciona i es podria utilitzar tambe per els enemics, encara que segurament el miri de canvia per algo mes eficient.
    A part per a fer les provas he creat el objecte jugador pero apart dels inputs per moures esta buit.

---
+++ cute_ass_hell/Assets/script/Jugador.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            Moviment.Moures(-10, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            Moviment.Moures(10, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            Moviment.Moures(0, 10);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            Moviment.Moures(0, -10);
+        }
+    }
+}
---
+++ cute_ass_hell/Assets/script/Moviment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour {
+
+    public GameObject personatge;
+    public static GameObject personatge2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personatge2 = personatge;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void Moures(int x, int y) {
+        personatge2.transform.position = personatge2.transform.position + new Vector3(x * Time.deltaTime, y * Time.deltaTime, 0);
+    }
+
+    public static void MouresX()
+    {
+        Moures(10, 0);
+    }
+
+    public static void MouresY()
+    {
+        Moures(0, 10);
+    }
+}
