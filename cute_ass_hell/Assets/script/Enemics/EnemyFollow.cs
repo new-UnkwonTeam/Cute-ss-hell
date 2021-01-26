68ef5ade094c3d9ada26f0d81c8f85ce7ff018ac
Cynthia <cintia.canela.7e3@itb.cat>
2021-01-26 16:54:03 +0100

    Ya funciona el seguimiento de los enemigos

---
+++ cute_ass_hell/Assets/script/Enemics/EnemyFollow.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private List<Rigidbody2D> EnemyRBs;
+    public float speed;
+    private Transform playerPos;
+
+    private float repelRange = .5f;
+    private Rigidbody2D rb;
+    void Awake()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+
+        if(EnemyRBs == null)
+        {
+            EnemyRBs = new List<Rigidbody2D>();
+        }
+
+        EnemyRBs.Add(rb);
+    }
+
+    private void OnDestroy()
+    {
+        EnemyRBs.Remove(rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, playerPos.position) > 0.9f)
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 repelForce = Vector2.zero;
+        foreach(Rigidbody2D enemy in EnemyRBs)
+        {
+            if(enemy == rb)
+                continue;
+
+            if(Vector2.Distance(enemy.position, rb.position) < - repelRange)
+            {
+                Vector2 repelDir =  (rb.position - enemy.position).normalized;
+                repelForce += repelDir;
+            }
+        }
+    }
+}
