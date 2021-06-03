6091df6024093df9328be0877ee41a4641e1a903
SocGrane3 <sergi.granell.7e3@itb.cat>
2021-06-03 20:53:42 +0200

    scenes updates

---
+++ cute_ass_hell/Assets/script/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void loadScene(int scene)
+    {
+        Debug.Log("pasa por aqui");
+        SceneManager.LoadScene(scene);
+    }
+
+    public void exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit(); 
+#endif
+    }
+}
