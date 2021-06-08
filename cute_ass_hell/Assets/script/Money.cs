a70666e6161cc3cd5c63dd5f470759a5e2184594
Eloi Alsina i Blanch <eloi.alsina.7e2@itb.cat>
2021-06-08 10:53:09 +0200

    Coins

---
+++ cute_ass_hell/Assets/script/Money.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    public int moneyAmount;
+
+    [SerializeField]
+    public Text coinCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinCounter.text = "Monedes:" + moneyAmount.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Coin>())
+        {
+            moneyAmount += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+}
